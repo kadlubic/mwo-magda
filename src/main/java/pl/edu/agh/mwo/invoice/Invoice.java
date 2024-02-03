ce943644e7b09f83ab18e22abf1c4d7e64b3ae32
kadlubic <mkadlubicka@student.agh.edu.pl>
2024-02-03 12:12:11 +0100

    Testy do dokończenia

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,30 +1,39 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
-import java.util.Collection;
+import java.sql.Array;
+import java.util.*;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-    private Collection<Product> products;
+    private Collection<Product> products = new ArrayList<>(); //kolecja i lista są interfejsami, więc musi być arraylist
+    private Map<Product, Integer> quantities;
 
     public void addProduct(Product product) {
-        // TODO: implement
+        this.products.add(product);
+
     }
 
     public void addProduct(Product product, Integer quantity) {
+        this.products.put(product, quantity);
         // TODO: implement
     }
 
-    public BigDecimal getSubtotal() {
-        return null;
+    public BigDecimal getNetPrice() {
+        BigDecimal netPrice = BigDecimal.ZERO;
+        for (Product product : this.products) {
+            netPrice = netPrice.add(product.getPrice());
+        }
+
+        return netPrice;
     }
 
     public BigDecimal getTax() {
-        return null;
+        return BigDecimal.ZERO;
     }
 
-    public BigDecimal getTotal() {
-        return null;
+    public BigDecimal getGrossPrice() {
+        return BigDecimal.ZERO;
     }
 }
